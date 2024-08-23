77f5ab6b71fc8b951bdec7043d0688cdc48e7ee3
Mariona Nadal <70766382+DSRschool@users.noreply.github.com>
2024-08-23 18:11:09 +0200

    Vídeo 3 - Antes

---
+++ 03_documentarSwagger/src/main/java/com/example/documentarSwagger/Cosa.java
@@ -0,0 +1,5 @@
+package com.example.documentarSwagger;
+
+public record Cosa (long id, String nombre) {
+
+}
---
+++ 03_documentarSwagger/src/test/java/com/example/documentarSwagger/DocumentarSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.documentarSwagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentarSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
