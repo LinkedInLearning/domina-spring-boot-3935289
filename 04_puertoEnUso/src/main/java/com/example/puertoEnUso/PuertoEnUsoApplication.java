cd4c9a75fb7355ca002556aebdef6151622cda15
Mariona Nadal <70766382+DSRschool@users.noreply.github.com>
2024-08-23 18:26:30 +0200

    Vídeo 4 - Después

---
+++ 04_puertoEnUso/src/main/java/com/example/puertoEnUso/PuertoEnUsoApplication.java
@@ -0,0 +1,13 @@
+package com.example.puertoEnUso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuertoEnUsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuertoEnUsoApplication.class, args);
+	}
+
+}
