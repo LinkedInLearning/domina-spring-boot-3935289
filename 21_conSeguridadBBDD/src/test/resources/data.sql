8a0d7b6f9679a054db956d7989b33bf0dddea0c6
Mariona Nadal <70766382+DSRschool@users.noreply.github.com>
2024-08-26 12:38:33 +0200

    Vídeo 21 - Después

---
+++ 21_conSeguridadBBDD/src/test/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO verdura (id, nombre, precio, troceable) VALUES ('2001', 'Tomate H2 Test', '3.82', false);
+INSERT INTO verdura (id, nombre, precio, troceable) VALUES ('2002', 'Calabaza H2 Test', '4.82', true);
+INSERT INTO verdura (id, nombre, precio, troceable) VALUES ('2003', 'Lechuga H2 Test', '2.82', false);
+ 
+INSERT INTO users (id, username, password, role, enabled) VALUES (2001, 'admin', '$2a$10$pktXnUlLW7EPtFNElALMsOLe9mNVquZGUmdRLqWSbnGTrqP0r2DQ2', 'ROLE_ADMIN', 1); -- password
+INSERT INTO users (id, username, password, role, enabled) VALUES (2002, 'carmen', '$2a$10$ImPwXJsXWkvh7IZzG5jyAezLkVannAM5hdl99jVatqjZLZ7Q3we6G', 'ROLE_USER', 1); -- lechuguita123
+-- contraseñas generadas con https://bcrypt-generator.com/
